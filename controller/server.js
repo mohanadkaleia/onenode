9779ce85e74649eb0ba68740dd4d28f3c3510816
Mohanad Kaleia <ms.kaleia@gmail.com>
2017-12-16 18:58:10 -0600

    Work on client side

--- controller/server.js
+++ controller/server.js
@@ -1,67 +1,80 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
+const WebSocketServer = require('websocket').server;
 const io  = require('socket.io').listen(5001);
 const dl  = require('delivery');
+const publicIp = require('public-ip');
+const winston = require('winston');
 
+// Configuration
+const socket = 9001;
 
-// List all files inside a folder
-testFolder = './test/'
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-})
+// Create log file
+winston.add(winston.transports.File, { filename: 'log.txt' });
 
-// Send a file over the browser
-http.createServer(function(request, response) {
-    var filePath = path.join(__dirname, 'test/test.txt');
-    var stat = fs.statSync(filePath);
-
-    response.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
-    });
-
-    var readStream = fs.createReadStream(filePath);
-    // We replaced all the event handlers with a simple call to readStream.pipe()
-    readStream.pipe(response);
-})
-.listen(2000);
-
-// Receive a file from another server
-io.sockets.on('connection', function(socket){
-
-  var delivery = dl.listen(socket);
-  delivery.on('receive.success',function(file){
-
-    fs.writeFile(file.name, file.buffer, function(err){
-      if(err){
-        console.log('File could not be saved: ' + err);
-      }else{
-        console.log('File ' + file.name + " saved");
-      };
-    });
-  });
+var server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
 });
 
-var socket = require('socket.io-client')('http://127.0.0.1:5001');
+server.listen(socket, function () {
+    winston.info((new Date()) + ' Server is listening on port ' + socket);
+});
 
-socket.on( 'connect', function() {
-  console.log( "Sockets connected" );
+var wsServer = new WebSocketServer({
+    httpServer: server, // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
 
-  delivery = dl.listen( socket );
-  delivery.connect();
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
 
-  delivery.on('delivery.connect',function(delivery){
-    delivery.send({
-      name: 'test.txt',
-      path : './test/test.txt'
+wsServer.on('request', function (request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        winston.info((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    winston.info((new Date()) + ' Connection from origin ' + request.origin + ' is accepted!');
+
+
+    var connection = request.accept(null, request.origin);
+    var isAuthorized = false;
+
+    // On message
+    connection.on('message', function (message) {
+        // var json = JSON.parse(message.utf8Data);
+        var delivery = dl.listen(socket);
+        delivery.on('receive.success',function(file) {
+
+          fs.writeFile(file.name, file.buffer, function(err) {
+            if(err) {
+              console.log('File could not be saved: ' + err);
+            } else {
+              console.log('File ' + file.name + " saved");
+            };
+          });
+        });
     });
 
-    delivery.on('send.success',function(file){
-      console.log('File sent successfully!');
+    // On close
+    connection.on('close', function (reasonCode, description) {
+        winston.info((new Date()) + ' station ' + connection.remoteAddress + ' disconnected.');
     });
-  });
+});
 
+
+publicIp.v4().then(ip => {
+	// console.log(ip);
+	//=> '46.5.21.123'
 });
