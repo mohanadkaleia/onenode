d8180ffd039bddc15f237b23c5ef26813a4bf925
Mohanad Kaleia <ms.kaleia@gmail.com>
2018-02-28 21:54:55 -0600

    Finalize the draft version of the UI

---
+++ client/src/components/Item.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <i v-if="status === 'online'" class="fas fa-file online" :title="name"></i>
+    <i v-else-if="status === 'offline'" class="fas fa-file offline" :title="name"></i>
+    <i v-else-if="status ==='local'" class="fas fa-file local" :title=name></i>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['name', 'status'],
+    data () {
+      return {
+        name
+      }
+    },
+    computed () {
+    }
+  }
+</script>
+
+<style>
+i {
+  font-size: 56px;
+  cursor: pointer;
+}
+
+i:hover.offline {
+  color: #F99157;
+}
+.online {
+  color: #5fb4b3;
+}
+
+.offline {
+  color: #ec5f66;
+}
+
+.local {
+  color: #6598cc;
+}
+
+i:hover.local {
+  color: #C594C5;
+}
+
+i:hover.online {
+  color: #99C794;
+}
+
+
+</style>
---
+++ client/src/components/Logo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="container-fluid logo">
+      <div class="row">
+        <div class="col">
+          <div class="square-top"></div>
+        </div>
+
+      </div>
+      <div class="row">
+          <div class="col offset-md-2">
+              <div class="circle"></div>
+          </div>
+          <div class="col"></div>
+          <div class="col">
+              <div class="circle"></div>
+          </div>
+          <div class="col"></div>
+          <div class="col"></div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+.circle {
+  display: inline-block;
+  width: 75px;
+  height: 75px;
+  background-color: #EC7263;
+  border-radius: 100%;
+}
+
+.logo-line {
+  margin: 15px 0 30px;
+
+  span {
+    display: inline-block;
+    height: 10px;
+    margin: 0 5px;
+    background-color: #C06162;
+    border-radius: 5px;
+
+    &:first-child {
+      width: 50px;
+    }
+
+    &:last-child {
+      width: 80px;
+    }
+  }
+}
+
+.square-top {
+  display: inline-block;
+  position: relative;
+  width: 60px;
+  height: 60px;
+  background-color: #FEBE7E;
+  z-index: 1;
+}
+
+.logo {
+  margin-top: 50px;
+}
+</style>
