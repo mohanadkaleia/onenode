2c9a7f082366a55f1352fe57cb28c2bfc10881ce
Mohanad Kaleia <ms.kaleia@gmail.com>
2019-06-21 23:39:44 -0700

    Fixing the server app

--- server/controllers/file_manager.js
+++ server/controllers/file_manager.js
@@ -1,17 +1,14 @@
-const chokidar = require('chokidar');
-const path_module = require('path');
-const datetime = require('node-datetime');
-const events = require('events');
+const chokidar = require("chokidar");
+const path_module = require("path");
+const datetime = require("node-datetime");
+const events = require("events");
 const eventEmitter = new events.EventEmitter();
 
 // Define the files path
-const data_path = 'data/';
-
-
-
+const data_path = "data/";
 
 // Initialize watcher.
-var watcher = chokidar.watch(data_path, {
+let watcher = chokidar.watch(data_path, {
   ignored: /(^|[\/\\])\../,
   ignoreInitial: false,
   persistent: true,
@@ -24,35 +21,35 @@ var watcher = chokidar.watch(data_path, {
 
 // Add event listeners.
 watcher
-  .on('add', path => {
-    console.log(`File ${path} has been added`)
+  .on("add", path => {
+    console.log(`File ${path} has been added`);
   })
-  .on('change', path => console.log(`File ${path} has been changed`))
-  .on('unlink', path => console.log(`File ${path} has been removed`))
-  .on('addDir', path => console.log(`Directory ${path} has been added`))
-  .on('unlinkDir', path => console.log(`Directory ${path} has been removed`))
-  .on('error', error => console.log(`Watcher error: ${error}`))
-  .on('ready', () => console.log('Initial scan complete. Ready for changes'));
+  .on("change", path => console.log(`File ${path} has been changed`))
+  .on("unlink", path => console.log(`File ${path} has been removed`))
+  .on("addDir", path => console.log(`Directory ${path} has been added`))
+  .on("unlinkDir", path => console.log(`Directory ${path} has been removed`))
+  .on("error", error => console.log(`Watcher error: ${error}`))
+  .on("ready", () => console.log("Initial scan complete. Ready for changes"));
 
 // Watch add event
-watcher.on('add', (path, stats) => {
+watcher.on("add", (path, stats) => {
   // TODO: check the file in the database before adding it
   file_name = path_module.basename(path);
   file_path = path_module.dirname(path);
 
   var dt = datetime.create();
-  var current_date = dt.format('Y-m-d H:M:S');
+  var current_date = dt.format("Y-m-d H:M:S");
 
-  console.log(current_date + ' ' + file_path + ' has been added');
+  console.log(current_date + " " + file_path + " has been added");
 });
 
 // Change event
-watcher.on('change', (path, stats) => {
+watcher.on("change", (path, stats) => {
   console.log(`File ${path} has been changed`);
 });
 
 // Delete event
-watcher.on('unlink', (path, stats) => {
+watcher.on("unlink", (path, stats) => {
   // TODO: delete the file from the database when it is changed
   file_name = path_module.basename(path);
   file_path = path_module.dirname(path);
